83d3827ef27ac2342de6681e8b54d7f299e9a618
Debora Amorim <debora.amorim@sonner.com.br>
2021-02-22 18:35:59 -0300

    Filter #01

---
+++ funções/filter_1.js
@@ -0,0 +1,9 @@
+/**
+ * A função filter aplicada a um array, vai gerar um novo array de acordo com
+ * a condição estabelecida.
+ * 
+ */
+
+ [1, 2, 3].filter(fn);
+
+//  fn deve retornar um booleano, quando true, o elemente fará parte do array resultante
