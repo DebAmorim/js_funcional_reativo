a79f33851572fa63d7f4d37c4396951fe874d242
Debora Amorim <debora.amorim@sonner.com.br>
2021-02-22 10:42:54 -0300

    Map #01

---
+++ funções/map_1.js
@@ -0,0 +1,7 @@
+
+/* A função map tem o objetivo de modificar um array por meio de uma função 
+* passada como argumento.
+* O resultado de uma função map terá sempre o mesmo tamanho do array original.
+*/
+
+[1,2,3].map(fn)
