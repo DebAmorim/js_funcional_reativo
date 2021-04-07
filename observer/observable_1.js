4f9d58701361298fd9737890458ad7599a07d57c
Debora Amorim <debora.amorim@sonner.com.br>
2021-04-07 07:56:29 -0300

    Observable

---
+++ observer/observable_1.js
@@ -0,0 +1,6 @@
+/*
+ Vantagem do observable em relação a Promise:
+ -Reusável: a Promise é resolvida uma única vez
+ -Stream de dados: sequência de dados, dados contínuos
+ -Operadores: funções para trabalhar com os dados
+*/
